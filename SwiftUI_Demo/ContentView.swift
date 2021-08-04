f047ffdac528fbec3b0453eaca7c30bf01da6c6b
Deep Panwala <deepjp@infusionanalysts.com>
2021-08-04 17:39:37 +0530

    Initial Commit

---
+++ SwiftUI_Demo/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo
+//
+//  Created by iOS Dev2 on 03/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        NavigationView{
+            VStack {
+                NavigationLink(
+                    destination: NextVC()) {
+                    Text("Next")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 300.0, height: 50.0)
+                        .background(Color(UIColor.black))
+                    
+                }
+            }
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Demo/NextVC.swift
@@ -0,0 +1,20 @@
+//
+//  NextVC.swift
+//  SwiftUI_Demo
+//
+//  Created by iOS Dev2 on 04/08/21.
+//
+
+import SwiftUI
+
+struct NextVC: View {
+    var body: some View {
+        Text("Second View!")
+    }
+}
+
+struct NextVC_Previews: PreviewProvider {
+    static var previews: some View {
+        NextVC()
+    }
+}
